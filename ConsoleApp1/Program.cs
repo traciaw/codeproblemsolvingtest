0d8b5ba20ea95a034b19bb83ffb93233233b66f1
[traciaw] <traciawee@gmail.com>
2022-09-28 17:17:49 +0800

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Add two numbers with decimal and round up to nearest int
+            double a = 2.3;
+            double b = 1.9;
+            int c = (int)Math.Ceiling(a + b);
+            int d = (int)Math.Floor(a + b);
+            Console.WriteLine("round up to int: " + c);
+            Console.WriteLine("round to int: " + d);
+
+            //findMedian n within array list
+
+            int[] n= {0,1,2,3,4,5,6};
+            int ArrayLength = n.Length;
+            int MedianPoint = ArrayLength / 2;
+            Console.WriteLine("Median n: " + n[MedianPoint]);
+            
+        }
+    }
+    
+}
