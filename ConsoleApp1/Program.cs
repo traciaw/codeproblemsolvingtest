a1c04292e2ceedf55c27f6beb09ef7ed5d88ed8c
[traciaw] <traciawee@gmail.com>
2022-10-03 19:49:13 +0800

    first commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -16,12 +16,33 @@ namespace ConsoleApp1
             Console.WriteLine("round to int: " + d);
 
             //findMedian n within array list
-
-            int[] n= {0,1,2,3,4,5,6};
+            int[] n= {7,6,0,1,2,3,4};
+            Array.Sort(n);
             int ArrayLength = n.Length;
             int MedianPoint = ArrayLength / 2;
             Console.WriteLine("Median n: " + n[MedianPoint]);
             
+            //sequence sum
+            int i=0; //0, 1, 2, 3, 4, 5 = 15
+            int j=5; //4, 3, 2, 1, 0 = 10
+            int k=1; //total shld be: 15 + 10 + 1 = 26
+
+            int sum = 0;
+            for (i = 0; i<= j; i++)
+            {
+                    sum +=i; 
+            }
+
+            Console.WriteLine("sum of i = " +sum);
+            int sum2 = 0;
+            for (j=5;j>i;j--)
+            {
+                sum2 +=j; 
+            }
+            Console.WriteLine("sum of j = " + sum2);
+
+
+
         }
     }
     
